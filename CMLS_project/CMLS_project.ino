4c8e72d2d08a73bc66af134b40cf7ca8e69daced
pepebocio <jorge.cuarterobo@gmail.com>
2025-05-17 15:37:32 +0200

    Arduino

--- CMLS_project/CMLS_project.ino
+++ CMLS_project/CMLS_project.ino
@@ -15,6 +15,9 @@ int calLed=2; //Dig Pin to calibrate red and white
 int redV=0;
 int whiteV=0;
 
+int pots[12]={};
+int leds[6]={};
+
 void setup() {
   Serial.begin(9600); 
   pinMode(4,OUTPUT);
@@ -25,19 +28,77 @@ void setup() {
 void loop() {
   measurePot();
   measureLeds();
-  Serial.println();
+  Serial.print("<");
+  int waves[4]={0,0,0,0};
+  if(leds[3]!=0){
+    waves[leds[3]-1]=pots[5];
+  }
+  if(leds[5]!=0){
+    waves[leds[5]-1]=pots[4];
+  }
+  Serial.print(waves[0]);
+  Serial.print(",");
+  Serial.print(waves[1]);
+  Serial.print(",");
+  Serial.print(waves[2]);
+  Serial.print(",");
+  Serial.print(waves[3]);
+  Serial.print(",");
+  Serial.print(pots[2]);
+  Serial.print(",");
+  Serial.print(pots[1]);
+  Serial.print(",");
+  Serial.print(pots[0]);
+  Serial.print(",");
+  Serial.print(pots[3]);
 
+  int fx[4]={0,0,0,0};
+  if(leds[1]!=0){
+    fx[leds[1]-1]=pots[8];
+  }
+  if(leds[2]!=0){
+    fx[leds[2]-1]=pots[11];
+  }
+  Serial.print(",");
+  Serial.print(fx[0]);
+  Serial.print(",");
+  Serial.print(fx[1]);
+  Serial.print(",");
+  Serial.print(fx[2]);
+  Serial.print(",");
+  Serial.print(fx[3]);
+  Serial.print(",");
 
+  int filt[4]={0,0,0,0};
+  if(leds[4]!=0){
+    filt[leds[4]-1]=pots[7];
+  }
+  if(leds[0]!=0){
+    filt[leds[0]-1]=pots[10];
+  }
+  Serial.print(filt[0]);
+  Serial.print(",");
+  Serial.print(filt[1]);
+  Serial.print(",");
+  Serial.print(filt[2]);
+  Serial.print(",");
+  Serial.print(filt[3]);
+  Serial.print(",");
+  Serial.print(pots[9]);
+  Serial.print(",");
+  Serial.print(pots[6]);
+  Serial.println(">");
 }
 
 void measurePot(){
   for (int i=0;i<digPot;i++){
     PotLine(potPins5V[i]);
     for(int j=0;j<anPot;j++){
-      Serial.print(" Pot");
+      /*Serial.print(" Pot");
       Serial.print(i*anPot+j);
       Serial.print(": ");
-      Serial.print(analogRead(potAnPins[j]));
+      Serial.print(analogRead(potAnPins[j]));*/
+      pots[i*anPot+j]=1000-analogRead(potAnPins[j]);
     }
     delay(Ts);
     //Serial.println();
@@ -67,10 +128,11 @@ void measureLeds(){
   for (int i=0;i<digLed;i++){
     LedLine(ledPins[i]);
     for(int j=0;j<anLed;j++){
-      Serial.print(" Led");
+      /*Serial.print(" Led");
       Serial.print(i*anLed+j);
-      Serial.print(": ");
-      colorLed(ledAnPins[j]);
+      Serial.print(": ");*/
+      
+      leds[i*anLed+j]=colorLed(ledAnPins[j]);
     }
     delay(Ts);
     
@@ -105,13 +167,13 @@ void calibrateLed(){
 }
 
 
-void colorLed(int analogPin){
+int colorLed(int analogPin){
   double V=1023-analogRead(analogPin);
   V=(V-redV)/(1.0*(whiteV-redV));
   //Serial.print(V);
-  if(V<0.05) Serial.print(" RED   ");
-  else if (V<0.5) Serial.print(" GREEN ");
-  else if (V<0.96) Serial.print(" BLUE  ");
-  else if (V<1.5) Serial.print(" WHITE ");
-  else Serial.print(" DISCO ");
+  if(V<0.05) return 1;
+  else if (V<0.5) return 2;
+  else if (V<0.96) return 3;
+  else if (V<1.5) return 4;
+  else return 0;
 }
